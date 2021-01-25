4e47dcb0ef8fa910b38af9271645e0547b7b2933
Maximilian <max.hvesser@finn.no>
2021-01-25 18:43:59 +0100

    Add basic NavigationComponent implementation

--- app/src/main/java/no/mhl/kotdoc/ui/host/HostActivity.kt
+++ app/src/main/java/no/mhl/kotdoc/ui/host/HostActivity.kt
@@ -1,11 +1,14 @@
-package no.mhl.kotdoc
+package no.mhl.kotdoc.ui.host
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import no.mhl.kotdoc.R
 
-class MainActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_host)
     }
+
 }
---
+++ app/src/main/java/no/mhl/kotdoc/ui/main/MainFragment.kt
@@ -0,0 +1,20 @@
+package no.mhl.kotdoc.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import no.mhl.kotdoc.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
