19ccf899453db43cc92a4711b17a63f135c65808
Maximilian <max.hvesser@finn.no>
2021-02-05 13:57:48 +0100

    Added necessary navigation routing.

--- app/src/main/java/no/mhl/kotdoc/ui/Navigation.kt
+++ app/src/main/java/no/mhl/kotdoc/ui/Navigation.kt
@@ -1,10 +1,21 @@
 package no.mhl.kotdoc.ui
 
+import android.os.Bundle
+import androidx.annotation.MainThread
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.core.os.bundleOf
 import androidx.lifecycle.ViewModel
 import no.mhl.kotdoc.ui.ScreenName.DOCUMENTATION
 import no.mhl.kotdoc.ui.ScreenName.FAVORITES
 import no.mhl.kotdoc.ui.ScreenName.SETTINGS
 import no.mhl.kotdoc.ui.ScreenName.SEARCH
+import no.mhl.kotdoc.utils.getMutableStateOf
+import no.mhl.kotdoc.ui.Screen.Documentation
+import no.mhl.kotdoc.ui.Screen.Favorites
+import no.mhl.kotdoc.ui.Screen.Settings
+import no.mhl.kotdoc.ui.Screen.Search
 
 // region Screens Declaration
 enum class ScreenName { DOCUMENTATION, FAVORITES, SETTINGS, SEARCH }
@@ -18,11 +29,47 @@ sealed class Screen(val id: ScreenName) {
 // endregion
 
 // region Screen To Bundle
+private const val SIS_SCREEN = "sis_screen"
+private const val SIS_NAME = "screen_name"
+private const val SIS_POST = "post"
 
+private fun Screen.toBundle(): Bundle {
+    return bundleOf(SIS_NAME to id.name)
+}
+
+private fun Bundle.toScreen() = when (ScreenName.valueOf(getStringOrThrow(SIS_NAME))) {
+    DOCUMENTATION -> Documentation
+    FAVORITES -> Favorites
+    SETTINGS -> Settings
+    SEARCH -> Search
+}
+
+private fun Bundle.getStringOrThrow(key: String) =
+    requireNotNull(getString(key)) { "Missing key '$key' in $this" }
 // endregion
 
 // region Navigation View Model
-class NavigationViewModel() : ViewModel() {
+class NavigationViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+
+    var currentScreen: Screen by savedStateHandle.getMutableStateOf<Screen>(
+        key = SIS_SCREEN,
+        default = Documentation,
+        save = { it.toBundle() },
+        restore = { it.toScreen() }
+    )
+        private set
+
+    @MainThread
+    fun onBack(): Boolean {
+        val wasHandled = currentScreen != Documentation
+        currentScreen = Documentation
+        return wasHandled
+    }
+
+    @MainThread
+    fun navigateTo(screen: Screen) {
+        currentScreen = screen
+    }
 
 }
 // endregion
