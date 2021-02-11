bd9620c5a7944d7784a156f71a3e6d49f0829a07
Maximilian <max.hvesser@finn.no>
2021-02-11 15:55:51 +0100

    Added basic BottomNavigation implementation

--- app/src/main/java/no/mhl/kotdoc/ui/documentation/DocumentationScreen.kt
+++ app/src/main/java/no/mhl/kotdoc/ui/documentation/DocumentationScreen.kt
@@ -1,28 +1,52 @@
 package no.mhl.kotdoc.ui.documentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import no.mhl.kotdoc.R
 import no.mhl.kotdoc.ui.Screen
+import no.mhl.kotdoc.ui.theme.black
+import no.mhl.kotdoc.ui.theme.blackTransparent
 
+// region Tab Models
+enum class Sections(
+    val title: String,
+    val icon: Int
+) {
+    Documentation("Documentation", R.drawable.ic_article),
+    Favorites("Favorites", R.drawable.ic_favorite)
+}
+
+class TabContent(val section: Sections, val content: @Composable () -> Unit)
+// endregion
+
+// region Main Content
 @Composable
 fun DocumentationScreen(
     navigateTo: (Screen) -> Unit
 ) {
+    val documentationSection = TabContent(Sections.Documentation) {
+        Text("Documentation", Modifier.padding(16.dp))
+    }
+    val favoritesSection = TabContent(Sections.Favorites) {
+        Text("Favorites", Modifier.padding(16.dp))
+    }
+
+    val tabContent = listOf(documentationSection, favoritesSection)
+    val (currentSection, updateSection) = savedInstanceState { tabContent.first().section }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(id = R.string.app_name))
+                    Text(text = stringResource(id = R.string.documentation_title))
                 },
                 backgroundColor = MaterialTheme.colors.primary,
                 contentColor = Color.Black,
@@ -36,12 +60,44 @@ fun DocumentationScreen(
                     .fillMaxWidth()
                     .background(MaterialTheme.colors.background)
             ) {
-                Text(
-                    text = "Documentation Screen",
-                    modifier = Modifier
-                        .padding(16.dp)
-                )
+                TabContent(currentSection, updateSection, tabContent)
             }
         }
     )
 }
+// endregion
+
+// region Tab Content
+@Composable
+private fun TabContent(
+    currentSection: Sections,
+    updateSection: (Sections) -> Unit,
+    tabContent: List<TabContent>
+) {
+    val selectedTabIndex = tabContent.indexOfFirst { it.section == currentSection }
+
+    Column {
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+            tabContent[selectedTabIndex].content()
+        }
+        BottomNavigation(
+            elevation = 0.dp
+        ) {
+            tabContent.forEachIndexed { index, tabContent ->
+                BottomNavigationItem(
+                    icon = { Icon(vectorResource(id = tabContent.section.icon)) },
+                    label = { Text(tabContent.section.title) },
+                    selected = selectedTabIndex == index,
+                    onClick = {
+                        updateSection(tabContent.section)
+                    },
+                    unselectedContentColor = blackTransparent,
+                    selectedContentColor = black
+                )
+            }
+        }
+    }
+}
+// endregion
--- app/src/main/java/no/mhl/kotdoc/ui/theme/Color.kt
+++ app/src/main/java/no/mhl/kotdoc/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package no.mhl.kotdoc.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// region Branding
 val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+// endregion
 
+// region General
 val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+val blackTransparent = Color(0x80000000)
 val alabaster = Color(0xFFF9F9F9)
+// endregion
