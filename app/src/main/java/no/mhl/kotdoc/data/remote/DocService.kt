e3cb880b79968be21e0ce89c8b6aae6c5adda5cb
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2021-02-28 20:21:35 +0100

    Added test network call

--- app/src/main/java/no/mhl/kotdoc/data/remote/DocService.kt
+++ app/src/main/java/no/mhl/kotdoc/data/remote/DocService.kt
@@ -1,7 +1,15 @@
 package no.mhl.kotdoc.data.remote
 
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Streaming
+import retrofit2.http.Url
+
 interface DocService {
 
-
+    @Streaming
+    @GET
+    fun getMarkdownForFile(@Url file: String): Call<ResponseBody>
 
 }
--- app/src/main/java/no/mhl/kotdoc/repository/DocRepository.kt
+++ app/src/main/java/no/mhl/kotdoc/repository/DocRepository.kt
@@ -1,6 +1,10 @@
 package no.mhl.kotdoc.repository
 
 import no.mhl.kotdoc.data.remote.DocService
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
 class DocRepository
@@ -8,5 +12,19 @@ class DocRepository
 constructor(
     private val docService: DocService
 ) {
+
+    fun getRemoteFile(fileName: String) {
+        docService.getMarkdownForFile(fileName).enqueue(object: Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                // On failure
+                val t = call
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                // On response
+                val t = call
+            }
+        })
+    }
 
 }
