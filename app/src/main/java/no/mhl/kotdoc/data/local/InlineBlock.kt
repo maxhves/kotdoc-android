9dc8092f56b70ae18bda8ace43f452cf956db78b
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2021-03-14 17:47:08 +0100

    Added classes ready for inline parsing

---
+++ app/src/main/java/no/mhl/kotdoc/data/local/InlineBlock.kt
@@ -0,0 +1,25 @@
+package no.mhl.kotdoc.data.local
+
+// region Base Inline Block
+open class InlineBlock(
+    open var content: String
+)
+// endregion
+
+// region Inline Blocks
+data class Bold(
+    override var content: String
+) : InlineBlock(content)
+
+data class Italic(
+    override var content: String
+) : InlineBlock(content)
+
+data class Code(
+    override var content: String
+) : InlineBlock(content)
+
+data class Link(
+    override var content: String
+) : InlineBlock(content)
+// endregion
---
+++ app/src/main/java/no/mhl/kotdoc/data/local/InlineParser.kt
@@ -0,0 +1,12 @@
+package no.mhl.kotdoc.data.local
+
+class InlineParser {
+
+    // region Parse Inline Blocks
+    fun parseToInlineBlocks(paragraph: String): List<InlineBlock> {
+        val blocks = mutableListOf<InlineBlock>()
+        return blocks
+    }
+    // endregion
+
+}
